0dacf85d8d9c656cc5c78eac371728fc9c07f6d8
azizcanakkaya <53472340+azizcanakkaya@users.noreply.github.com>
2019-08-25 20:55:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element =
+       <div>
+          <input type="text" name="E-mail"><br>
+          <input type="text" name="Password">
+          <button>LOG IN</button>
+       </div>
+
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
